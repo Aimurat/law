91e9e364f9af61af2e13ed066fd76d4ef8310042
bobur554396 <bobur.muhsimbaev@gmail.com>
2021-02-10 17:47:09 +0600

    I-Search changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  //apiUrl: 'http://188.127.36.17:8001'
-   apiUrl: 'http://10.1.30.31:8001'
+  apiUrl: 'http://188.127.36.17:8001'
+  //  apiUrl: 'http://10.1.30.31:8001'
   // apiUrl: 'http://185.217.183.107:8001'
 
 };
